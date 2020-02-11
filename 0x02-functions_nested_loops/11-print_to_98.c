6e12270e2e4b060b260a4052ee4591b6399cfd43
Andres Giraldo <andresfg95@gmail.com>
2020-02-11 15:28:55 +0000

    fix for 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,19 +9,20 @@ void print_to_98(int n)
 {
 	if (n > 98)
 	{
-		while (n >= 98)
+		while (n > 98)
 		{
 			printf("%d, ", n);
 			n--;
 		}
 	}
 
-	else if (n <= 98)
+	else
 	{
-		while (n <= 98)
+		while (n < 98)
 		{
 			printf("%d, ", n);
 			n++;
 		}
-	}
+	}	
+	printf("%d\n", n);
 }
