b36631d5f66e631b90a17befbac9466a1cd4b60e
Andres Giraldo <andresfg95@gmail.com>
2020-02-13 17:41:21 +0000

    Including task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+*
+*
+*
+*/
+
+int main (void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf("Fizz "); 
+		}
+		else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	putchar('\n');
+	return (0); 
+}
