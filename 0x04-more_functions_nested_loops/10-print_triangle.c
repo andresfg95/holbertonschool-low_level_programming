5820462edda2b7e1143ef61ad4fad433b64e32dd
Andres Giraldo <andresfg95@gmail.com>
2020-02-14 02:43:35 +0000

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,39 @@
+#include "holberton.h"
+
+/**
+* print_triangle - prints triangle followed by new line.
+* @size: size of triangle
+* Return: nothing
+**/
+
+void print_triangle(int size)
+{
+	if (size > 0)
+	{
+		int i, space, pound;
+
+		count = 0;
+
+		while (i < size)
+		{
+			space = i;
+
+			while (space < size - 1)
+			{
+				_putchar(' ');
+				space++;
+			}
+			pound = 0;
+
+			while (hashs <= i)
+			{
+				_putchar('#');
+				pound++;
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
+	else
+		_putchar('\n');
+}
